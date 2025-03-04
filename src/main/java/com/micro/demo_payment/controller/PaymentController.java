8b504afa40bd6c683950f5e11dd26f6030e6b164
alaaalbadry <albadryalaaa@gmail.com>
2025-03-04 04:55:54 +0200

    create payment controller

---
+++ src/main/java/com/micro/demo_payment/controller/PaymentController.java
@@ -0,0 +1,32 @@
+package com.micro.demo_payment.controller;
+
+
+import com.micro.demo_payment.model.Payment;
+import com.micro.demo_payment.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @GetMapping("list")
+    public ResponseEntity<List<Payment>> getPayments() {
+        List<Payment> payments = paymentService.findAll();
+        return new ResponseEntity<>(payments, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Payment> createPayment(@RequestBody Payment payment) {
+        Payment createdPayment = paymentService.createPayment(payment);
+        return new ResponseEntity<>(createdPayment, HttpStatus.CREATED);
+    }
+
+}
