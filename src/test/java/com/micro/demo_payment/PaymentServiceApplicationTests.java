11a5bdaa021cf0de43aed5375b7bed8bfd1fef91
alaaalbadry <albadryalaaa@gmail.com>
2025-03-04 04:54:29 +0200

    create test class

---
+++ src/test/java/com/micro/demo_payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.demo_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
